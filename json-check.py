230b0172f07c585f5f67d307b41aad6f0588f622
Nazarbek <nazbekktl@gmail.com>
2017-03-29 20:11:55 +0000

    counting words in each json

---
+++ json-check.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+data = []
+
+letters = ['а', 'ә', 'б', 'в', 'г', 'ғ', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'қ', 
+            u'л', 'м', 'н', 'о', 'ө', 'п', 'р', 'с', 'т', 'у', 'ұ', 'ү', 'ф', 
+            u'х', 'ц', 'ч', 'ш', 'ы', 'і', 'э', 'ю', 'я']
+len_ = 0
+
+for i in letters:
+    uri = 'parsed-words/letter-' + i + '.json'
+    with open(uri, 'r') as f:
+        data = json.load(f)
+        len_ += len(data)
+        
+print len_
+print len(letters)
